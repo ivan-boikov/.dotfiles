cbffe0ea857c27453e72231efa340dc266642715
ivan-boikov <ik.boikov.public@protonmail.com>
2022-07-10 16:44:04 +0200

    1

--- stow.sh
+++ stow.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+echo "Cleaning old mimeapps.list"
 rm ~/.config/mimeapps.list
+echo "Cleaning old user-dirs.dirs"
 rm ~/.config/user-dirs.dirs
 
+echo "Stowing configs"
 stow -R $(ls -d */)
 
+echo "Installing ProtonMail Bridge OpenRC service file"
 doas ln -s "$HOME/.local/bin/protonmail-bridge" "/etc/init.d/protonmail-bridge"
